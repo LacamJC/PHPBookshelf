4d28f53a644915ffb5a9e20e8be001aa99bd12fe
joao-ramajo <joaoramajo744@gmail.com>
2025-08-04 02:01:16 -0300

    refactor: adiciona tipagem de parametros e de retorno em `Route.php`

--- Api/Core/Router.php
+++ Api/Core/Router.php
@@ -6,29 +6,27 @@ class Router
 {
     private $routes = [];
 
-    // Adicionando uma rota GET
-    public function get($path, $handler)
+    public function get(string $path, string $handler): void
     {
         $this->routes['GET'][$path] = $handler;
     }
 
-    // Adicionando uma rota POST
-    public function post($path, $handler)
+    public function post(string $path, string $handler): void
     {
         $this->routes['POST'][$path] = $handler;
     }
 
-    public function delete($path, $handler)
+    public function delete(string $path, string $handler): void
     {
         $this->routes['DELETE'][$path] = $handler;
     }
 
-    public function put($path, $handler)
+    public function put(string $path, string $handler): void
     {
         $this->routes['PUT'][$path] = $handler;
     }
 
-    public function dispatch()
+    public function dispatch(): void
     {
         $url = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
         $url = $url === '' ? '/' : $url;
@@ -37,29 +35,22 @@ class Router
         foreach ($this->routes[$method] as $route => $handler) {
             $pattern = preg_replace('/\{([a-z]+)\}/', '(?P<$1>[^/]+)', $route);
             $pattern = "@^$pattern$@";
-            
+
             if (preg_match($pattern, $url, $matches)) {
                 $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
                 [$controllerClass, $action] = explode('@', $handler);
-                
+
                 $fullControllerClass = "Api\\Controllers\\$controllerClass";
-      
+
                 if (class_exists($fullControllerClass)) {
                     $controller = new $fullControllerClass();
-
-                    
                     if (method_exists($controller, $action)) {
-
-
                         $controller->$action($params ?? []);
                         return;
                     }
                 }
-
-                die('nao existe');
             }
         }
-
         header("HTTP/1.0 404 Not Found");
         echo "Página não encontrada";
     }
--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
-require_once dirname(__DIR__, 1) . '/routes/routes.php';
 require_once dirname(__DIR__, 1) . '/config/bootstrap.php';
+require_once dirname(__DIR__, 1) . '/routes/routes.php';
 
 $router->dispatch();
